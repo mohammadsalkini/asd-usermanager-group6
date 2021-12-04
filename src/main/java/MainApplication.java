09ac4ba157b90184589a337a9d851702ac30287c
Christian Hüttler <christian.huettler@stud.fh-campuswien.ac.at>
2021-12-04 16:47:32 +0100

    Create render/main page functionality #16: Implemented new Class UserView with -renderPage and -mainPage methods;

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -1,6 +1,6 @@
 public class MainApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // TODO: implement interaction with UserView Class
     }
 }
