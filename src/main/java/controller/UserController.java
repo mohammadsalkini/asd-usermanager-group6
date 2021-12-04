4d47a127dc8711c7cb803a74aa8fb8322c4a63eb
mohammad Alsalkini <eng.salkini@hotmail.com>
2021-12-04 21:24:16 +0100

    Add createAccount end point  #4

---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import model.User;
+import service.UserService;
+
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    public User createAccount (String username, String password, String firstName, String lastName) {
+        return userService.createNewUser(username, firstName, lastName, password);
+    }
+}
