9a630fc8b6668ac05af05bb731b2c3ed4204c2d6
mohammad Alsalkini <eng.salkini@hotmail.com>
2021-12-04 21:20:26 +0100

    Add createNewUser method implementation  #4

---
+++ src/main/java/service/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package service;
+
+import data.DBConnector;
+import model.User;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService{
+
+    private final DBConnector connect;
+
+    public UserServiceImpl(DBConnector connect) {
+        this.connect = connect;
+    }
+
+    @Override
+    public User createNewUser(String username, String firstName, String lastName, String password) {
+        User user = connect.selectUserByUsernameAndPassword(username, password);
+        if (user == null) {
+            return connect.addUser(username, firstName, lastName, password);
+        }
+        return null;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteAccount(String username) {
+        return false;
+    }
+}
