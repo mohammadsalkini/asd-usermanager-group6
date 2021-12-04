ddd263a47485a353e0381fb65f7ded4fdf1fe01f
Christian Hüttler <christian.huettler@stud.fh-campuswien.ac.at>
2021-12-04 17:04:41 +0100

    Implement logic in MainApplication class #20

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -1,6 +1,13 @@
+import view.UserView;
+import java.security.NoSuchAlgorithmException;
+
 public class MainApplication {
 
     public static void main(String[] args) {
-        // TODO: implement interaction with UserView Class
+        try {
+            UserView.renderPage();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
 }
